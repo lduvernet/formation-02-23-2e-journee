b48dd88f4d97abe3057d3053ee20423091a4f0ed
lduvernet <laurent.duvernet@interieur.gouv.fr>
2023-02-28 12:57:47 +0000

    commit documentation des fonctions

---
+++ script/functions.R
@@ -0,0 +1,32 @@
+decennie_a_partir_annee <- function(annee) {
+  return(annee - annee %% 10)
+}
+
+
+#' Statistiques descriptives
+#' 
+#' @description
+#' `stat_des` renvoie une statistique descriptive des données
+#'
+#' @details
+#' L'argument `donnees` doit contenir un vecteur de données.
+#' L'argument `stat` peut être "moyenne" (par défaut), "ecart-type" ou "variance"
+#' 
+#' @param donnees Un vecteur.
+#' @param stat Un nom de fonction.
+#' @return Le nombre résultat de la fonction appliquée au vecteur.
+#'  
+#' @examples
+#' stat_des(rnorm(100))
+#' stat_des(rpois(100, lambda = 2), stat = "variance")
+stat_des <- function(donnees, stat = "moyenne", ...) {
+  if (stat == "moyenne") {
+    res <- mean(donnees, na.rm = TRUE, ...)
+  } else if (stat == "ecart-type" || stat == "sd") {
+    res <- sd(donnees, na.rm = TRUE, ...)
+  } else if (stat == "variance") {
+    res <- var(donnees, na.rm = TRUE, ...)
+  }
+  return(res)
+}
+
--- script/script.R
+++ script/script.R
@@ -6,25 +6,7 @@ library(dplyr)
 
 token  = yaml::read_yaml("secrets.yaml")$api_token
 
-
-# DEFINITION DES FONCTIONS ----
-
-
-decennie_a_partir_annee <- function(annee) {
-  return(annee - annee %% 10)
-}
-
-fonction_de_stat_agregee <- function(a, b = "moyenne", ...) {
-  if (b == "moyenne") {
-    x <- mean(a, na.rm = TRUE, ...)
-  } else if (b == "ecart-type" || b == "sd") {
-    x <- sd(a, na.rm = TRUE, ...)
-  } else if (b == "variance") {
-    x <- var(a, na.rm = TRUE, ...)
-  }
-  return(x)
-}
-
+source("script/functions.R", encoding = "UTF-8")
 
 
 # IMPORT DES DONNEES ----
@@ -62,8 +44,8 @@ df3 %>%
 
 summarise(group_by(df, aged), n())
 
-fonction_de_stat_agregee(df %>% filter(sexe == "Homme") %>% pull(aged))
-fonction_de_stat_agregee(df %>% filter(sexe == "Femme") %>% pull(aged))
+stat_des(df %>% filter(sexe == "Homme") %>% pull(aged))
+stat_des(df %>% filter(sexe == "Femme") %>% pull(aged))
 
 
 
